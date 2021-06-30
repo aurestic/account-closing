9035ced8450998b19dd2ad773e3f6cbf377306d9
almumu <almudena@aurestic.es>
2021-06-30 12:13:35 +0200

    nuevo módulo backport de la 12 para cierre de ejercicio fiscal, con algunas mejoras y un data con la plantilla de cierre

---
+++ account_fiscal_year_closing/models/__init__.py
@@ -0,0 +1,5 @@
+
+from . import account_fiscalyear_closing_abstract # noqa
+from . import account_fiscalyear_closing # noqa
+from . import account_fiscalyear_closing_template # noqa
+from . import account_move # noqa
